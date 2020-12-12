1596ca24141fcb014920f7c4037a32bda453601f
Rohan Port <rohan.port@gmail.com>
2020-12-12 12:01:06 +1100

    Made model rendering conditional on OS

--- models/basic-reinforcement/basic-reinforcement.py
+++ models/basic-reinforcement/basic-reinforcement.py
@@ -1,4 +1,5 @@
 # generate random integer values
+import os
 from random import seed
 from random import randint
 
@@ -8,13 +9,20 @@ from pettingzoo.mpe import simple_v2
 env = simple_v2.env(max_cycles=100)
 env.reset()
 seed(1)
+
 for agent in env.agent_iter():
-    env.render()
-    time.sleep(0.1)
+    if (os.getenv('SHOW_DISPLAY')):
+        env.render()
+        time.sleep(0.1)
+    
+    
     observation, reward, done, info = env.last()
     print(observation, reward, done, info)
     if (not done):
     	env.step(randint(0,4))
     else:
     	env.step(None)
-env.close()
+
+
+if (os.getenv('SHOW_DISPLAY')):
+        env.close()
